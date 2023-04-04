e4c300630576d62727440dcf4e53dc700e946a31
apricotn18 <apricotn18@gmail.com>
2023-04-05 01:11:46 +0900

    230405 docs

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 app.listen(process.env.PORT || 8000);
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/docs'));
 
 app.use((req, res, next) => {
-	res.sendFile(__dirname + '/dist/index.html');
+	res.sendFile(__dirname + '/docs/index.html');
 });
