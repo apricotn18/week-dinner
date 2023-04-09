09413c26df87de5f965202e335782d69f13e6f79
apricotn18 <apricotn18@gmail.com>
2023-04-09 12:42:41 +0900

    230409

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 app.listen(process.env.PORT || 8000);
-app.use(express.static(__dirname + '/docs'));
+app.use(express.static(__dirname + '/week-dinner/docs'));
 
 app.use((req, res, next) => {
-	res.sendFile(__dirname + '/docs/index.html');
+	res.sendFile(__dirname + '/week-dinner/docs/index.html');
 });
