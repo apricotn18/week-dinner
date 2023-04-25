dd8603e6e3afc649c8877eaf80e2414573c3eb5f
apricotn18 <apricotn18@gmail.com>
2023-04-25 15:07:20 +0900

    230425

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const app = express();
-app.listen(process.env.PORT || 8000);
-app.use(express.static(__dirname + '/week-dinner/docs'));
+const http = require('http');
+const fs = require('fs');
 
-app.use((req, res, next) => {
-	res.sendFile(__dirname + '/week-dinner/docs/index.html');
+const server = http.createServer((req, res) => {
+	fs.readFile('./dist/index.html', 'UTF-8', (err, data) => {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		res.end();
+	});
 });
+
+server.listen(process.env.PORT || 8000);
