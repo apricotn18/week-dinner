28946c78efa4f7fe39c2e89aba6eb860db5234f8
apricotn18 <apricotn18@gmail.com>
2023-09-26 12:50:19 +0900

    エラー解消

--- pages/component/footer/Footer.js
+++ pages/component/footer/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './footer.scss';
+import style from './footer.module.scss';
 
 export default function Footer () {
 	return (
-		<footer className="footer">
-			<div className="footer-wrapper">
-				<p><small className="footer-text">&copy;{new Date().getFullYear()} KYOKO NOZAKI</small></p>
-				<p className="footer-text">
+		<footer className={style.footer}>
+			<div className={style.footer_wrapper}>
+				<p><small className={style.footer_text}>&copy;{new Date().getFullYear()} KYOKO NOZAKI</small></p>
+				<p className={style.footer_text}>
 					<a href="https://webservice.rakuten.co.jp/" target="_blank">
 						Supported by Rakuten Developers
 					</a>
--- pages/component/header/Header.js
+++ pages/component/header/Header.js
@@ -1,36 +1,53 @@
 import React, { useState } from 'react';
+import Link from 'next/link';
 import image_logo from './logo.png';
 import Image from 'next/image';
-import './header.scss';
+import style from './header.module.scss';
 
 export default function Header () {
 	const [modalShow, setModalShow] = useState(false);
+	const [modalStyle, setModalStyle] = useState({display: 'none'});
 
-	const toggleModal = () => {
-		setModalShow(!modalShow);
-	};
+	const handleModal = {
+		close: () => {
+			setModalShow(false);
+			setModalStyle({display: 'none'});
+		},
+		toggle: () => {
+			setModalShow(!modalShow);
+			setModalStyle({display: modalShow ? 'none' : 'block'});
+		}
+	}
 
 	return (
-		<header className="header">
-			<div className="header-wrapper">
-				<a href="/" className="header-img">
-					<h1><Image src={image_logo} width="178" alt="week dinner" /></h1>
-				</a>
-				<button className="header-menu_button" onClick={toggleModal}></button>
+		<header className={style.header}>
+			<div className={style.header_wrapper}>
+				<Link href="/" legacyBehavior>
+					<a className={style.header_img} onClick={handleModal.close}>
+						<h1><Image src={image_logo} width="144" alt="week dinner" /></h1>
+					</a>
+				</Link>
+				<button className={style.header_menuButton} onClick={handleModal.toggle}></button>
 			</div>
 
-			<div className={modalShow ? 'menu_modal is-open' : 'menu_modal'}>
-				<div className="menu_modal-bg" onClick={toggleModal}></div>
-				<div className="menu_modal_wrapper">
-					<ul className="menu_modal-list">
-						<li className="menu_modal-item">
-							<a href="/" className="menu_modal-link">トップ</a>
+			<div className={style.modal} style={modalStyle}>
+				<div className={style.modal_bg} onClick={handleModal.close}></div>
+				<div className={style.modal_wrapper}>
+					<ul className={style.modal_list}>
+						<li className={style.modal_item}>
+							<Link href="/" legacyBehavior>
+								<a className={style.modal_link} onClick={handleModal.close}>トップ</a>
+							</Link>
 						</li>
-						<li className="menu_modal-item">
-							<a href="/list" className="menu_modal-link menu_modal-link--buy">お買い物リスト</a>
+						<li className={style.modal_item}>
+							<Link href="/list" legacyBehavior>
+								<a className={style.modal_link} onClick={handleModal.close}>お買い物リスト</a>
+							</Link>
 						</li>
-						<li className="menu_modal-item">
-							<a href="/ranking" className="menu_modal-link menu_modal-link--rank">ランキングからレシピ登録</a>
+						<li className={style.modal_item}>
+							<Link href="/ranking" legacyBehavior>
+								<a className={style.modal_link} onClick={handleModal.close}>ランキングからレシピ登録</a>
+							</Link>
 						</li>
 					</ul>
 				</div>
