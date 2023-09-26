44b14b52042919ef3e5c3146469c584d070c2c4a
野崎 杏子 <nozaki_kyoko@waku-2.com>
2023-09-26 12:51:55 +0900

    index 実装

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'image.space.rakuten.co.jp',
+				port: '',
+				pathname: '**',
+			},
+		],
+	},
+}
 
 module.exports = nextConfig
---
+++ pages/component/index/index.tsx
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from 'react';
+import RakutenRecipeAPI from "../../../assets/js/RakutenRecipeAPI";
+import style from "./index.module.scss";
+
+type Recipe = {
+	recipeTitle?: string;
+	recipeCost?: string;
+	recipeDescription?: string;
+	recipeIndication?: string;
+	recipeUrl?: string;
+	foodImageUrl?: string;
+	recipeMaterial?: string[];
+}
+type ModalStyle = {
+	top: '44px'|'100%';
+}
+
+export default function Page() {
+	const rakutenRecipeAPI = new RakutenRecipeAPI();
+	const division = ["今日","明日","明後日","3日後","4日後","5日後","6日後"];
+	const [recipe, setRecipe] = useState<Recipe[]>([{},{},{},{},{},{},{}]);
+	const [modalRecipe, setModalRecipe] = useState<Recipe>(recipe[0]);
+	const [modalStyle, setModalStyle] = useState<ModalStyle>({top: '100%'});
+
+	// レシピ更新
+	useEffect(() => {
+		rakutenRecipeAPI.initFetch()
+			.then((result) => {
+				setRecipe(result);
+			}).catch(() => {
+				alert('読み込みに失敗しました。通信状況を確認してください');
+			});
+	}, []);
+
+	/**
+	 * レシピモーダル
+	 * @return {void}
+	*/
+	const handleModal = {
+		open: (
+			e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+		) => {
+			// レシピ書き換え
+			const index: string = e.currentTarget.dataset.dateNum || '0';
+			setModalRecipe(recipe[Number.parseInt(index)]);
+			setModalStyle({top: '44px'});
+			document.body.style.overflow = 'hidden';
+		},
+		close: () => {
+			setModalStyle({top: '100%'});
+			document.body.style.overflow = '';
+		}
+	}
+
+	/**
+	 * レシピを変更
+	 * @return {void}
+	*/
+	const updateSingleRecipe = (
+		e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+	) => {
+		const target = e.currentTarget;
+		// ボタンを非活性にする（時間に複数回通信するとエラーとなるため）
+		target.disabled = true;
+		// レシピ書き換え
+		const index: string = e.currentTarget.dataset.dateNum || '0';
+		rakutenRecipeAPI.fetch(Number.parseInt(index))
+			.then((result) => {
+				setRecipe(result);
+			}).catch(() => {
+				alert('読み込みに失敗しました。通信状況を確認してください');
+			});
+		// ボタンを活性に戻す
+		setTimeout(() => {
+			target.disabled = false;
+		}, 1500);
+	}
+
+	return (
+		<>
+			<section className="wrapper">
+				<div className="contents">
+					<ul className={style.recipe}>
+						{recipe.map((item: Recipe, i: number) => {
+							{if (i >= 7) return}
+							return (
+							<li className={style.recipe} key={i}>
+								<button type="button" className={style.recipe_button} data-date-num={i} onClick={handleModal.open}>
+									<div className={style.recipe_image} style={{backgroundImage: `url(${item.foodImageUrl})`}}></div>
+									<div className={style.recipe_info}>
+										<div className={style.recipe_head}>
+											<p className={style.recipe_division}>{division[i]}のレシピ</p>
+											<p className={style.recipe_time}>{item.recipeIndication}</p>
+											<p className={style.recipe_price}>{item.recipeCost}</p>
+										</div>
+										<p className={style.recipe_title}>{item.recipeTitle}</p>
+									</div>
+								</button>
+								{item.foodImageUrl &&
+									<div className={style.recipe_update}>
+										<button type="button" data-date-num={i} onClick={updateSingleRecipe}></button>
+									</div>
+								}
+							</li>
+							)
+						})}
+					</ul>
+				</div>
+			</section>
+
+			{/* レシピモーダル */}
+			<div className={style.modal} style={modalStyle}>
+				<div>
+					<div className={style.modal_content}>
+						<div className={style.modal_head}>
+							<p className={style.modal_title}>{modalRecipe.recipeTitle}</p>
+							<div className={style.modal_close} onClick={handleModal.close}></div>
+						</div>
+						<div>
+							<img className={style.modal_image} src={modalRecipe.foodImageUrl||''} alt="レシピ画像" />
+						</div>
+						<div className={style.modal_info}>
+							<div className={style.modal_info_head}>
+								<p className={style.modal_time}>{modalRecipe.recipeIndication}</p>
+								<p className={style.modal_price}>{modalRecipe.recipeCost}</p>
+							</div>
+							<p className={style.modal_description}>{modalRecipe.recipeDescription}</p>
+							{modalRecipe.recipeMaterial &&
+								<table className="table">
+									<thead>
+										<tr><th>材料</th></tr>
+									</thead>
+									<tbody>
+										{modalRecipe.recipeMaterial.map((item, i: number) => <tr key={i}><td>{item}</td></tr>)}
+									</tbody>
+								</table>
+							}
+							<div>
+								<a href={modalRecipe.recipeUrl} className={style.modal_button} target="_blank">
+									詳しいレシピを見る
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			{/* レシピモーダル */}
+		</>
+	)}
