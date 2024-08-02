dd38723f8ad46c463fb330bacfa64f5e18f45810
kyoko nozaki <nozaki_kyoko@waku-2.com>
2024-08-02 18:49:14 +0900

    モーダル開閉

---
+++ pages/component/RecipeModal/RecipeModalComp.tsx
@@ -0,0 +1,46 @@
+import style from "./recipeModal.module.scss";
+import { Recipe } from "../../../assets/js/type";
+
+type Props = {
+	item: Recipe;
+	isOpen: boolean;
+	handleOpenModal: React.MouseEventHandler<HTMLButtonElement>;
+};
+
+export default function recipeModal ({ item, isOpen, handleOpenModal }: Props) {
+	return (
+		<div className={`${style.wrapper} ${isOpen && style.isOpen}`}>
+			<div className={style.content}>
+				<div className={style.head}>
+					<p className={style.title}>{item.recipeTitle}</p>
+					<button className={style.close} onClick={handleOpenModal}></button>
+				</div>
+				<div>
+					<img className={style.image} src={item.foodImageUrl||''} alt="レシピ画像" />
+				</div>
+				<div className={style.info}>
+					<div className={style.info_head}>
+						<p className={style.time}>{item.recipeIndication}</p>
+						<p className={style.price}>{item.recipeCost}</p>
+					</div>
+					<p className={style.description}>{item.recipeDescription}</p>
+					{item.recipeMaterial &&
+						<table className="table">
+							<thead>
+								<tr><th>材料</th></tr>
+							</thead>
+							<tbody>
+								{item.recipeMaterial.map((item, i: number) => <tr key={i}><td>{item}</td></tr>)}
+							</tbody>
+						</table>
+					}
+					<div>
+						<a href={item.recipeUrl} className={style.button} target="_blank">
+							もっと詳しいレシピを見る
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
